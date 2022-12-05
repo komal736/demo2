99684097aeb5554b2e289f514f48d73b5d0c60db
Komal Do <komaldokania22@gmail.com>
2022-12-06 02:05:51 +0530

    add_l

---
+++ demo.js
@@ -0,0 +1,18 @@
+
+
+/*document.getElementById('header-title').style.borderBottom='solid 3px #000'
+document.getElementById('header-title').style.color='green';*/
+
+/*var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++)
+  items[i].style.fontWeight='bold';*/
+
+var li=document.getElementsByTagName('li');
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow'
+li[4].style.backgroundColor='yellow'
